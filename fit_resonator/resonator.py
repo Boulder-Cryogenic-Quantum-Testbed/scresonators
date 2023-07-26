1670e82ffb289f94d2d92be066b5523cce432c48
Scottito <niha4803@colorado.edu>
2023-07-26 12:11:56 -0600

    Splitting of 'Sdata' into two separate files 'fit' and 'plot' with other minor improvements

--- fit_resonator/resonator.py
+++ fit_resonator/resonator.py
@@ -2,10 +2,11 @@
 import datetime
 import attrs
 import attr
+import os
 import numpy as np
 
-import fit_resonator.functions as ff
-import fit_resonator.Sdata as fs
+import fit_resonator.cavity_functions as ff
+import fit_resonator.fit as fit
 
 
 class FitMethod(object):
@@ -107,6 +108,17 @@ class FitMethod(object):
             elif method == 'CPZM':
                 self.func = ff.cavity_CPZM
 
+class ResonatorData:
+    # Simple container for data
+    def __init__(self,
+                 freqs: np.ndarray,
+                 amps: np.ndarray,
+                 phases: np.ndarray,
+                 linear_amps: np.ndarray):
+        self.freqs = freqs
+        self.amps = amps
+        self.phases = phases
+        self.linear_amps = linear_amps
 
 @attr.define(init=True)
 class Resonator:  # Object is auto-initialized with @attr annotation
@@ -131,8 +143,8 @@ class Resonator:  # Object is auto-initialized with @attr annotation
     """
 
     filepath: str = None
-    data: fs.VNASweep or np.ndarray = None
-    databg: fs.VNASweep or np.ndarray = None
+    data: ResonatorData = None
+    databg: ResonatorData = None
     method_class: FitMethod = None
     name: str = ''
     date: datetime.datetime = None
@@ -143,66 +155,66 @@ class Resonator:  # Object is auto-initialized with @attr annotation
     background: str = None
     background_array: np.ndarray = None
     plot_extra: bool = False
-    preprocess_method: str = "linear"
+    preprocess_method: str = "circle"
+    power = 0
 
     # Store non-VNASweep forms of data passed in class construction as VNASweep objects.
     def __attrs_post_init__(self):
         if self.filepath is not None and self.data is None:
             self.from_file()
 
-        if self.data is not None and not isinstance(self.data, fs.VNASweep):
+        if self.data is not None and not isinstance(self.data, ResonatorData):
             self.from_columns(self.data.T[0], self.data.T[1], self.data.T[2])
 
         if self.background is not None and self.databg is None:
             self.init_background(filepath=self.background)
-        if self.background_array is not None and self.bg is None:
+        if self.background_array is not None and self.databg is None:
             self.init_background_array(self.background_array)
 
     def init_background(self, filepath=background, fscale=1e9):
         if self.background is None and filepath is not None:
             self.background = filepath
-        self.databg = fs.VNASweep.from_file(self.background, fscale)
+        self.databg = from_file(self.background, fscale)
 
     def init_background_array(self, bg_array=background_array):
         if self.background_array is None and bg_array is not None:
             self.background_array = bg_array
-        self.databg = fs.VNASweep.from_columns(self.background_array.T[0],
+        self.databg = from_columns(self.background_array.T[0],
                 self.background_array.T[1], self.background_array.T[2])
 
     def from_columns(self, freqs, amps=None, phases=None):
         # Allows for user to pass array variable alone
         if freqs is not None and amps is None and phases is None:
-            self.data = fs.VNASweep.from_columns(freqs.T[0], freqs.T[1],
-                                                 freqs.T[2])
+            self.data = from_columns(freqs.T[0], freqs.T[1], freqs.T[2])
         else:
-            self.data = fs.VNASweep.from_columns(freqs, amps, phases)
+            self.data = from_columns(freqs, amps, phases)
 
     def from_file(self, filepath=filepath, measurement=None, fscale=1e9):
         if self.filepath is None and filepath is not None:
             self.filepath = filepath
         if self.measurement is None and measurement is not None:
             self.measurement = measurement
-        self.data = fs.VNASweep.from_file(self.filepath, data_column=measurement, fscale=fscale)
+        self.data = from_file(self.filepath, data_column=measurement, fscale=fscale)
 
     def fit_method(self,
                    method: str,
-                   MC_iteration=None,
+                   MC_iteration: int = 5,
                    MC_rounds=100,
                    MC_weight='no',
                    MC_weightvalue=2,
                    MC_fix=[],
                    MC_step_const=0.6,
-                   manual_init=None,
-                   vary=None,
+                   manual_init: list = [],
+                   vary: bool = False,
                    preprocess_method: str = preprocess_method):
         self.method_class = FitMethod(method, MC_iteration, MC_rounds,
                 MC_weight, MC_weightvalue, MC_fix, MC_step_const, manual_init,
                 vary, preprocess_method)
 
     def fit(self):
-        fs.fit(self)
+        fit.fit(self)
 
-    def load_params(self, method: str, params: list, chi: any):
+    def load_params(self, method: str, params: np.ndarray, chi):
         """
         Loads model parameters for a corresponding fit technique.
 
@@ -216,54 +228,31 @@ class Resonator:  # Object is auto-initialized with @attr annotation
         if self.method is None:
             self.method = []
             self.fc = params[3]
-            if method == 'DCM':
-                self.method.append("DCM")
-                self.DCMparams = DCMparams(params, chi)
-                self.compare = ff.fit_raw_compare(self.freq, self.S21, self.DCMparams.all, 'DCM')
-            elif method == 'PHI':
-                self.method.append("PHI")
-                self.DCMparams = DCMparams(params, chi)
-                self.compare = ff.fit_raw_compare(self.freq, self.S21, self.DCMparams.all, 'DCM')
-            if method == 'DCM REFLECTION':
-                self.method.append("DCM REFLECTION")
-                self.DCMparams = DCMparams(params, chi)
-                self.compare = ff.fit_raw_compare(self.freq, self.S21, self.DCMparams.all, 'DCM')
-            elif method == 'INV':
-                self.method.append("INV")
+
+            assert method in ['DCM', 'PHI', 'DCM REFLECTION', 'INV', 'CPZM'], "Wrong Method, please input: PHI, DCM, INV or CPZM"
+            self.method.append(method)
+            if method == 'INV':
                 self.INVparams = INVparams(params, chi)
-                self.compare = ff.fit_raw_compare(self.freq, self.S21, self.INVparams.all, 'INV')
+                self.compare = ff.fit_raw_compare(self.data.freqs, self.data.amps, self.INVparams.all, method)
             elif method == 'CPZM':
-                self.method.append("CPZM")
                 self.CPZMparams = CPZMparams(params, chi)
             else:
-                print('Please input DCM, DCM REFLECTION, PHI, INV or CPZM')
+                self.DCMparams = DCMparams(params, chi)
+                self.compare = ff.fit_raw_compare(self.data.freqs, self.data.amps, self.DCMparams.all, method)
+                
         else:
             if method not in self.method:
                 self.method.append(method)
-
-                if method == 'DCM':
-                    self.method.append("DCM")
-                    self.DCMparams = DCMparams(params, chi)
-
-                elif method == 'PHI':
-                    self.method.append("PHI")
-                    self.DCMparams = DCMparams(params, chi)
-
-                elif method == 'DCM REFLECTION':
-                    self.method.append("DCM REFLECTION")
-                    self.DCMparams = DCMparams(params, chi)
-
-                elif method == 'INV':
-                    self.method.append("INV")
+                if method == 'INV':
                     self.INVparams = INVparams(params, chi)
-
                 elif method == 'CPZM':
-                    self.method.append("CPZM")
                     self.CPZMparams = CPZMparams(params, chi)
+                else:
+                    self.DCMparams = DCMparams(params, chi)
             else:
                 print("repeated load parameter")
 
-    def reload_params(self, method: str, params: list, chi: any):
+    def reload_params(self, method: str, params: np.ndarray, chi):
         """
         Reloads model parameters for a corresponding fit technique.
 
@@ -279,19 +268,19 @@ class Resonator:  # Object is auto-initialized with @attr annotation
             self.fc = params[3]
             if method == 'DCM REFLECTION':
                 self.DCMparams = DCMparams(params, chi)
-                self.compare = ff.fit_raw_compare(self.freq, self.S21, self.DCMparams.all, 'DCM')
+                self.compare = ff.fit_raw_compare(self.data.freqs, self.data.amps, self.DCMparams.all, 'DCM')
             elif method == 'INV':
 
                 self.INVparams = INVparams(params, chi)
-                self.compare = ff.fit_raw_compare(self.freq, self.S21, self.INVparams.all, 'INV')
+                self.compare = ff.fit_raw_compare(self.data.freqs, self.data.amps, self.INVparams.all, 'INV')
             elif method == 'CPZM':
                 self.method.append("CPZM")
                 self.CPZMparams = CPZMparams(params, chi)
         else:
             print('no')
 
-    def power_calibrate(self,
-                        p):  # optional input calibration: linear function of input S21(frequency) to sample cables, meas. at room temp.
+    def power_calibrate(self, p):  
+        # optional input calibration: linear function of input S21(frequency) to sample cables, meas. at room temp.
 
         assert (self.DCMparams is not None) or (self.INVparams is not None), 'Please load parameters first'
         p = np.array(p)
@@ -310,11 +299,194 @@ class Resonator:  # Object is auto-initialized with @attr annotation
             Qc = self.INVparams.Qc
             self.INVparams.num_photon = 2 * p / hbar / f ** 2 * Q ** 2 / Qc
 
+def from_columns(freqs, amps, phases):
+    """Load data from columns provided individually."""
+    linear_amps = 10 ** (amps / 20)
+    return ResonatorData(freqs=freqs, amps=amps, phases=phases, linear_amps=linear_amps)
+
+def from_file(filepath, data_column=None, fscale=1e9):
+    if data_column is not None:
+        s_col = data_column
+    else:
+        s_col = 1
+    filename, extension = os.path.splitext(filepath)
+    if extension.startswith('.s') and extension.endswith('p'):
+        try:
+            snp_file = open(filepath, 'r')
+        except OSError as e:
+            print(f'ERROR {e} when opening file')
+            print(f'Data file: {filepath} could not be found/read')
+            quit()
+        file, inline, options, frequency_units, data_format = header_parse(file=snp_file)
+        freqs, amps, phases, linear_amps = data_parse(s_col, inline, frequency_units, data_format, file, options)
+        if frequency_units == 'hz':
+            fscale = fscale / 1e9
+        elif frequency_units == 'mhz':
+            fscale = fscale / 1e3
+        elif frequency_units == 'khz':
+            fscale = fscale / 1e6
+        freqs = freqs / fscale
+
+        return ResonatorData(freqs, amps, phases, linear_amps)
+    elif 'txt' in extension or 'csv' in extension:
+        try:
+            txt_file = open(filepath, 'r')
+            file, line, options, frequency_units, data_format = header_parse(file=txt_file)
+            data_lines = []
+            while line:
+                if 'END' in line:
+                    break
+                data_lines.append(line)
+                line = file.readline().strip()
+            data = np.loadtxt(data_lines, delimiter=',')
+        except Exception as e:
+            print(f'Exception: **{e}** encountered when attempting to load data file as .txt/.csv')
+            print(f'Are you using a comma as your delimiter?')
+            quit()
+
+        freqs = data.T[0] / fscale
+        amps = data.T[1]
+        phases = data.T[2] * np.pi / 180
+        linear_amps = 10 ** (amps / 20)
+        return ResonatorData(freqs, amps, phases, linear_amps)
+    else:
+        print(f'File extension {extension} not supported.')
+        print(f'Please use .s2p, .s1p, .txt, or .csv')
+        quit()
+
+def header_parse(file):
+    data_format = None
+    frequency_units = None
+    comment_line = ['!']
+    option_line = ['#']
+    metadata = ['s21', 's11', 's12', 's22']
+    dformats = ['db', 'ma', 'ri']
+    nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '.']
+
+    options = []
+    inline = file.readline()
+    while any(comment in inline.lower() for comment in comment_line) \
+            or any(option_lead in inline for option_lead in option_line) \
+            or not any(number in inline.lower()[0] for number in nums):
+        if any(option_lead in inline.lower() for option_lead in option_line) \
+                or any(dformat in inline.lower() for dformat in dformats) \
+                or any(measure in inline.lower() for measure in metadata):
+            options.append(inline)
+
+        inline = file.readline()
+
+    for val in options:
+        if 'db' in val.lower():
+            data_format = 'db'
+        elif 'ma' in val.lower():
+            data_format = 'ma'
+        elif 'ri' in val.lower():
+            data_format = 'ri'
+
+        if 'hz' in val.lower():
+            frequency_units = 'hz'
+        elif 'khz' in val.lower():
+            frequency_units = 'khz'
+        elif 'mhz' in val.lower():
+            frequency_units = 'mhz'
+        elif 'ghz' in val.lower():
+            frequency_units = 'ghz'
+
+    return file, inline, options, frequency_units, data_format
+
+def data_parse(s_col, line, frequency_units, data_format, file, options):
+    row = line.split()
+    data_rows = [3, 4]
+    if len(row) == 0:
+        print("Data not found in file.")
+        quit()
+
+    if len(row) > 3:
+        # If too many rows, use info from header to pull correct column
+        # s_col has potential focus column
+        if isinstance(s_col, int):
+            data_rows[0] = s_col
+            # data_rows[1] = self.c_col + 1 VNASweep has no c_col
+        elif isinstance(s_col, (tuple, list, np.ndarray)):
+            data_rows[0] = s_col[0]
+            data_rows[1] = s_col[1]
+        elif isinstance(s_col, str):
+            for metadata in options:
+                if 'Measurements: ' in metadata:
+                    measurements = metadata.rsplit('Measurements: ')[1].strip('.:\n').lower().split(', ')
+                    idx = (measurements.index(s_col.lower()) * 2) + 1
+                    data_rows[0] = idx
+                    data_rows[1] = idx + 1
+                    break
+        else:
+            print("Could not interpret which data columns to use, using default")
+
+    freqs = np.array(float(row[0]))
+    if data_format == "db":
+        amps = np.array(float(row[data_rows[0]]))
+        phases = np.array(float(row[data_rows[1]]))
+        line = file.readline().strip()
+
+        while line:
+            row = line.split()
+            freqs = np.append(freqs, float(row[0]))
+            amps = np.append(amps, float(row[data_rows[0]]))
+            phases = np.append(phases, float(row[data_rows[1]]))
+            line = file.readline().strip()
+        phases = phases * np.pi / 180
+        linear_amps = 10 ** (amps / 20)
+
+    elif data_format == "ma":
+        linear_amps = np.array(float(row[data_rows[0]]))
+        phases = np.array(float(row[data_rows[1]]))
+        line = file.readline().strip()
+
+        while line:
+            row = line.split()
+            freqs = np.append(freqs, float(row[0]))
+            linear_amps = np.append(linear_amps, float(row[data_rows[0]]))
+            phases = np.append(phases, float(row[data_rows[1]]))
+            line = file.readline().strip()
+
+        phases = phases * np.pi / 180
+        amps = np.log10(linear_amps) * 20
+
+    elif data_format == "ri":
+        real = np.array(float(row[data_rows[0]]))
+        imaginary = np.array(float(row[data_rows[1]]))
+        line = file.readline().strip()
+
+        while line:
+            row = line.split()
+            freqs = np.append(freqs, float(row[0]))
+            real = np.append(real, float(row[data_rows[0]]))
+            imaginary = np.append(imaginary, float(row[data_rows[1]]))
+            line = file.readline().strip()
+        linear_amps = np.absolute(real + imaginary)
+        phases = np.angle(real + 1j * imaginary, deg=True)
+        amps = np.log10(linear_amps) * 20
+
+    else:
+        print("Data type in file not supported. Please use DB, MA, or RI.")
+        quit()
+
+    if frequency_units == "hz":
+        freqs = freqs / 10 ** 9
+    elif frequency_units == "khz":
+        freqs = freqs / 10 ** 6
+    elif frequency_units == "mhz":
+        freqs = freqs / 10 ** 3
+    elif frequency_units != "ghz":
+        print(
+            "Units for the frequency not found. Please include units for frequency in the header of the file.")
+    return freqs, amps, phases, linear_amps
+
 
 @attrs.define
 class DCMparams(object):  # DCM fitting results
     params: np.ndarray
     chi: float
+    num_photon: float = 0
 
     def __attrs_post_init__(self):
         self.Qc = self.params[2]
@@ -335,6 +507,7 @@ class DCMparams(object):  # DCM fitting results
 class INVparams(object):  # INV fitting results
     params: np.ndarray
     chi: float
+    num_photon: float = 0
 
     def __attrs_post_init__(self):
         self.Qc = self.params[2]
@@ -353,6 +526,7 @@ class INVparams(object):  # INV fitting results
 class CPZMparams(object):
     params: np.ndarray
     chi: float
+    num_photon: float = 0
 
     def __attrs_post_init__(self):
         self.Qc = self.params[2]
