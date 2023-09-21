9a09b28e274b2cee38926959097412244a157890
Scottito <nicholas.hardmanjr@colorado.edu>
2023-09-20 22:47:51 -0600

    change assert to exception

--- test/user_test.py
+++ test/user_test.py
@@ -33,6 +33,9 @@ my_resonator.filepath = './' # Path to fit output
 my_resonator.fit_method(fit_type, MC_iteration, MC_rounds=MC_rounds, MC_fix=MC_fix, 
                         manual_init=manual_init, MC_step_const=0.3)
 output_params, conf_array, error, init = my_resonator.fit(None)
-assert output_params == [306209.29520741466, 928387.8353093242, 4512506964.96403, -0.15431421139237836]
-assert conf_array == [588.1821370752295, 1685.6238136050524, 1634.941474343068, 1654.6028376782779, 0.0017515102820822026, 14.244794845581055]
-assert error == 0.00012610664079341314
+if output_params != [306209.29520741466, 928387.8353093242, 4512506964.96403, -0.15431421139237836]:
+    raise Exception("Different output parameters than expected in user_test.py")
+if conf_array != [588.1821370752295, 1685.6238136050524, 1634.941474343068, 1654.6028376782779, 0.0017515102820822026, 14.244794845581055]:
+    raise Exception("Different confidence array than expected in user_test.py")
+if error != 0.00012610664079341314:
+    raise Exception("Different error than expected in user_test.py")
