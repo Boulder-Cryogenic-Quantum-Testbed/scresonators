156aec4cdbe2ba1d356f2d882113044bdf7e01b9
zachmiles9 <zachmiles9@gmail.com>
2024-06-13 12:49:21 -0600

    TEST-small changes to comments

--- src/resonator.py
+++ src/resonator.py
@@ -3,13 +3,21 @@ from .fit_methods.fit_method import FitMethod
 
 class Resonator:
     def __init__(self, data=None):
+        """Initialize the resonator object"""
         self.data = data
         self.fitter = None
         self.fit_result = None
 
     def load_data(self, data):
-        """Load data into the resonator object."""
+        """Load data into the resonator object. This method is optional as data could have been loaded when the class instance was created"""
         self.data = data
+        #use pandas dataframes 
+        #tocsv
+        #fromcsv
+        #create method that 
+
+    def save_data(self, data):
+        #save data
 
     def set_fitting_strategy(self, strategy: FitMethod):
         """Set the fitting strategy with a FitMethod object."""
