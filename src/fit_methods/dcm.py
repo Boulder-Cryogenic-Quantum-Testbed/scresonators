8ed06d89d865ffef4f476d5667c3c823fcdf5c75
zachmiles9 <zachmiles9@gmail.com>
2024-07-19 16:19:05 -0600

    'find_initial_guess' works now with hard-coded values.

--- src/fit_methods/dcm.py
+++ src/fit_methods/dcm.py
@@ -17,46 +17,38 @@ class DCM(FitMethod):
     def abs_func(x, Q, Qc, f0, phi):
         return (((1-((Q/Qc*np.cos(phi) + 2*(Q**2)/f0*(x-f0)/Qc*np.sin(phi))/(1+4*(Q**2)/(f0**2)*((x-f0)**2))))**2) + (((2*(Q**2)/f0*(x-f0)/Qc*np.cos(phi) + Q/Qc*np.sin(phi))/(1+4*(Q**2)/(f0**2)*((x-f0)**2)))**2)) ** (0.5)
 
+    @staticmethod
+    def abs_func2(x, Q, Qc, f0, phi):
+        return np.abs(1 - Q / Qc * np.exp(1j * phi) / (1 + 1j * (x - f0) / f0 * 2 * Q))
+
     def create_model(self):
         """Creates an lmfit Model using the static func method."""
         model = lmfit.Model(self.func, independent_vars=['x'])
         return model
 
     def find_initial_guess(self, x: np.ndarray, y: np.ndarray) -> lmfit.Parameters: ## Redundant? There is a '_estimate_initial_parameters' method in Fitter class
-        param_guesses, _ = scipy.optimize.curve_fit(self.abs_func, x, abs(y))
+        Q, Qc = 1e5, 1e5
+        f0_idx = np.abs(y).argmin()
+        f0 = x[f0_idx]
+        phi = 0
+
+        init_guess_params = np.array([Q, Qc, f0, phi])
+        param_guesses, _ = scipy.optimize.curve_fit(self.abs_func2, x, abs(y), init_guess_params)
         print("Parameter guesses: ", param_guesses)
 
-        # # Use specific criteria or model to calculate initial guesses
-        # phi_guess = np.angle(-z_c)
-        # Q_guess = 1e4  # Placeholder guess
-        # Qc_guess = Q_guess / np.abs(y_adjusted[freq_idx])  # Example calculation
-
-        #Place initial guess intermediate variables into a dictionary
-        # guess_intermediate_var = {'circle center': z_c, 'Adjusted complex data': y_adjusted}
-
-        # param_guesses = self.form_initial_guess(x, Q_guess, Qc_guess, f0_guess, phi_guess)
+        Q_guess = param_guesses[0]
+        Qc_guess = param_guesses[1]
+        f0_guess = param_guesses[2]
+        phi_guess = param_guesses[3]
 
         # Create an lmfit.Parameters object to store initial guesses
         param_guesses = lmfit.Parameters()
-        # param_guesses.add('Q', value=Q_guess, min=1e3, max=1e6)
-        # param_guesses.add('Qc', value=Qc_guess, min=1e3, max=1e6)
-        # param_guesses.add('f0', value=f0_guess, min=f0_guess*0.9, max=f0_guess*1.1)
-        # param_guesses.add('phi', value=phi_guess, min=-np.pi, max=np.pi)
+        param_guesses.add('Q', value=Q_guess, min=1e3, max=1e6)
+        param_guesses.add('Qc', value=Qc_guess, min=1e3, max=1e6)
+        param_guesses.add('f0', value=f0_guess, min=f0_guess*0.9, max=f0_guess*1.1)
+        param_guesses.add('phi', value=phi_guess, min=-np.pi, max=np.pi)
 
         return param_guesses
-
-        # return param_guesses, guess_intermediate_var
-    
-    ## TODO Fix this functionality
-    # def form_initial_guess(self, x, Q_guess, Qc_guess, f0_guess, phi_guess):
-    #     # Create an lmfit.Parameters object to store initial guesses
-    #     param_guesses = lmfit.Parameters()
-    #     param_guesses.add('Q', value=Q_guess, min=1e3, max=1e6)
-    #     param_guesses.add('Qc', value=Qc_guess, min=1e3, max=1e6)
-    #     param_guesses.add('f0', value=f0_guess, min=f0_guess*0.9, max=f0_guess*1.1)
-    #     param_guesses.add('phi', value=phi_guess, min=-np.pi, max=np.pi)
-
-    #     return param_guesses
     
     def generate_highres_fit(self, x: np.ndarray, fit_params, num_fit_points=1000):
         
