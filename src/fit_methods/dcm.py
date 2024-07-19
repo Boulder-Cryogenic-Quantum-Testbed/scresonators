ba87a82986d28a5f026746324ac66c7c1e465202
zachmiles9 <zachmiles9@gmail.com>
2024-07-19 15:38:43 -0600

    New 'find_initial_guess' in DCM using scipy.optimize.curve_fit. Not working yet

--- src/fit_methods/dcm.py
+++ src/fit_methods/dcm.py
@@ -1,5 +1,6 @@
 import numpy as np
 import lmfit
+import scipy
 from .fit_method import FitMethod
 from ..utils import find_circle
 
@@ -12,24 +13,23 @@ class DCM(FitMethod):
         """DCM fit function."""
         return 1 - Q / Qc * np.exp(1j * phi) / (1 + 1j * (x - f0) / f0 * 2 * Q) ## absolute value of Qc actually
     
+    @staticmethod
+    def abs_func(x, Q, Qc, f0, phi):
+        return (((1-((Q/Qc*np.cos(phi) + 2*(Q**2)/f0*(x-f0)/Qc*np.sin(phi))/(1+4*(Q**2)/(f0**2)*((x-f0)**2))))**2) + (((2*(Q**2)/f0*(x-f0)/Qc*np.cos(phi) + Q/Qc*np.sin(phi))/(1+4*(Q**2)/(f0**2)*((x-f0)**2)))**2)) ** (0.5)
+
     def create_model(self):
         """Creates an lmfit Model using the static func method."""
         model = lmfit.Model(self.func, independent_vars=['x'])
         return model
-    
 
     def find_initial_guess(self, x: np.ndarray, y: np.ndarray) -> lmfit.Parameters: ## Redundant? There is a '_estimate_initial_parameters' method in Fitter class
-        x_c, y_c, r = find_circle(np.real(y), np.imag(y))
-        z_c = x_c + 1j * y_c
-        # Adjust y to the circle's reference frame
-        y_adjusted = y - (1 + z_c)
-    
-        # Use specific criteria or model to calculate initial guesses
-        phi_guess = np.angle(-z_c)
-        freq_idx = np.argmax(np.abs(y_adjusted))
-        f0_guess = x[freq_idx]
-        Q_guess = 1e4  # Placeholder guess
-        Qc_guess = Q_guess / np.abs(y_adjusted[freq_idx])  # Example calculation
+        param_guesses, _ = scipy.optimize.curve_fit(self.abs_func, x, abs(y))
+        print("Parameter guesses: ", param_guesses)
+
+        # # Use specific criteria or model to calculate initial guesses
+        # phi_guess = np.angle(-z_c)
+        # Q_guess = 1e4  # Placeholder guess
+        # Qc_guess = Q_guess / np.abs(y_adjusted[freq_idx])  # Example calculation
 
         #Place initial guess intermediate variables into a dictionary
         # guess_intermediate_var = {'circle center': z_c, 'Adjusted complex data': y_adjusted}
@@ -38,10 +38,10 @@ class DCM(FitMethod):
 
         # Create an lmfit.Parameters object to store initial guesses
         param_guesses = lmfit.Parameters()
-        param_guesses.add('Q', value=Q_guess, min=1e3, max=1e6)
-        param_guesses.add('Qc', value=Qc_guess, min=1e3, max=1e6)
-        param_guesses.add('f0', value=f0_guess, min=f0_guess*0.9, max=f0_guess*1.1)
-        param_guesses.add('phi', value=phi_guess, min=-np.pi, max=np.pi)
+        # param_guesses.add('Q', value=Q_guess, min=1e3, max=1e6)
+        # param_guesses.add('Qc', value=Qc_guess, min=1e3, max=1e6)
+        # param_guesses.add('f0', value=f0_guess, min=f0_guess*0.9, max=f0_guess*1.1)
+        # param_guesses.add('phi', value=phi_guess, min=-np.pi, max=np.pi)
 
         return param_guesses
 
